afd1e81705032d250989876bc557b55dca92f9b8
Kim Arsinenko <124343207+Arsinenko@users.noreply.github.com>
2024-11-04 20:09:08 +0300

    commit

--- AvaloniaApplication1/Login.axaml.cs
+++ AvaloniaApplication1/Login.axaml.cs
@@ -22,17 +22,18 @@ public partial class Login : UserControl
         string password = PasswordTextBox.Text;
         using (var db = new CollegedbContext()) 
         {
-            int result = Convert.ToInt32(db.Users.FirstOrDefault(u => u.Login == login && u.Password == password));
+            User result = db.Users.FirstOrDefault(u => u.Login == login && u.Password == password);
             
             if (result == null)
             {
-                MessageBoxManager.GetMessageBoxStandard(title: "Error", text: "Login or password is incorrect. User not found!");
+                MessageBoxManager.GetMessageBoxStandard(title: "Error", text: "Login or password is incorrect. User not found!").ShowAsync();
             }
-            else if (result == 1)
+            else
             {
-                MessageBoxManager.GetMessageBoxStandard(title: "Success", text: "You are logged in!");
+                MessageBoxManager.GetMessageBoxStandard(title: "Success", text: "You are logged in!").ShowAsync();
                 //change page to AdminPage 
-                var mainWindow = Parent.
+                var mainWindow = this.VisualRoot as MainWindow;
+                mainWindow?.NavigeteTo(new AdminPage());
             }
         }
     }
--- AvaloniaApplication1/UsersDtg.axaml.cs
+++ AvaloniaApplication1/UsersDtg.axaml.cs
@@ -21,17 +21,6 @@ public partial class UsersDtg : UserControl
             var users = db.Users.ToList();
             DataGridUsr.ItemsSource = users;
         }
-        using (var db = new CollegedbContext())
-        {
-            var users = db.Users.ToList();
-            DataGridUsr.ItemsSource = users;
-        }
-
-        DataGridUsr.Columns.Clear(); // Очистите существующие колонки перед добавлением новых
-        DataGridUsr.Columns.Add(new DataGridTextColumn {Header = "ID", Binding = new Avalonia.Data.Binding("Id")});
-        DataGridUsr.Columns.Add(new DataGridTextColumn {Header = "Name", Binding = new Avalonia.Data.Binding("Name")});
-        DataGridUsr.Columns.Add(new DataGridTextColumn {Header = "Login", Binding = new Avalonia.Data.Binding("Login")});
-        DataGridUsr.Columns.Add(new DataGridTextColumn {Header = "Password", Binding = new Avalonia.Data.Binding("Password")});
-        DataGridUsr.Columns.Add(new DataGridTextColumn {Header = "IdRole", Binding = new Avalonia.Data.Binding("Idrole")});
+       
     }
 }
